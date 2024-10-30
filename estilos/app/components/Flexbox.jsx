14aa07b60ba41d588fb4b4bb9e620834dd70ac8b
Ricardo Palacios <rpmaya@gmail.com>
2024-10-30 18:56:41 +0100

    Update Flexbox.jsx

--- estilos/app/components/Flexbox.jsx
+++ estilos/app/components/Flexbox.jsx
@@ -10,14 +10,16 @@ export default function Flexbox() {
     </div>
     {/** Mantienen su ancho definido con flex-wrap
      * con gap-4, ponemos un espacio entre ellos
+     * con grow les permitiremos crecer si tienen espacio para hacerlo
+     * con shrink-0 se mantiene la relación
     */}
     <div className="flex flex-wrap gap-4">
-      <div className="card">1</div>
-      <div className="card">2</div>
-      <div className="card">3</div>
-      <div className="card">4</div>
-      <div className="card">5</div>
-      <div className="card">6</div>
+      <div className="card grow shrink-0">1</div>
+      <div className="card grow shrink-0">2</div>
+      <div className="card grow shrink-0">3</div>
+      <div className="card grow shrink-0">4</div>
+      <div className="card grow shrink-0">5</div>
+      <div className="card grow shrink-0">6</div>
     </div>
     
     </>
